2d833dc722ea0a00c36f699b0ed53fc4fda895f8
isabella3412 <izzybella17@icloud.com>
2018-06-22 15:08:31 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
 # book.rb
 # Add you Book class here
+class Book
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author= (author)
+    @author = author
+  end
+
+end
